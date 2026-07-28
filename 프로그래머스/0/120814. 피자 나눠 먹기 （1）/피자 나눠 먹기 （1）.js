e850d467debfa7ba1ea3fbedf5a3c51dd3773be8
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-07-28 21:59:07 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.06 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,9 +1,3 @@
 function solution(n) {
-    if(n < 8) return 1
-    if(n % 7 === 0) {
-        return Math.trunc(n / 7)
-    } else {
-        return Math.trunc(n / 7) + 1
-    }
-    
+  return Math.ceil(n / 7)  
 }
