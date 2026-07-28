aab547c03d85688af44d6a56db9416def43d0aec
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-07-28 15:47:18 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.10 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,9 +1,11 @@
-function solution(num_list, n) {
-    let answer = []
-    for(let i = 0; i < num_list.length; i++){
-        if(i % n === 0){
-            answer.push(num_list[i])
-        }
-    }
-    return answer;
-}
+// function solution(num_list, n) {
+//     let answer = []
+//     for(let i = 0; i < num_list.length; i++){
+//         if(i % n === 0){
+//             answer.push(num_list[i])
+//         }
+//     }
+//     return answer;
+// }
+
+const solution = (list, n) => list.filter((_, idx) => idx % n === 0)
