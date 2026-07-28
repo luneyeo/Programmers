9982a502e0e54b1d7d7faf8a5d4594883b564b7d
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-07-28 16:04:30 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.06 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
@@ -1,10 +1,14 @@
-function solution(my_string, target) {
-    var answer = 0;
-    const result = my_string.search(target)
-    if(result === -1){
-        answer = 0
-    } else {
-        answer = 1
-    }
-    return answer;
+// function solution(my_string, target) {
+//     var answer = 0;
+//     const result = my_string.search(target)
+//     if(result === -1){
+//         answer = 0
+//     } else {
+//         answer = 1
+//     }
+//     return answer;
+// }
+
+const solution = (string, target) => {
+    return string.includes(target) ? 1 : 0
 }
