5bcd40a5903bc998544b6e53ab646cc1933f405c
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-07-29 13:20:12 +0900

    [level 0] Title: 배열의 평균값, Time: 0.07 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,8 +1,11 @@
-function solution(numbers) {
-    let sum = 0;
-    numbers.forEach((n) => {
-        sum += n
-    })
-    return sum / numbers.length
+// function solution(numbers) {
+//     let sum = 0;
+//     numbers.forEach((n) => {
+//         sum += n
+//     })
+//     return sum / numbers.length
     
+// }
+const solution = (numbers) => {
+    return numbers.reduce((acc, num) => acc + num, 0) / numbers.length
 }
