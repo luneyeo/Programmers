d09ee40429e894a1310118b125b44c2361ae8236
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-07-29 13:27:11 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.07 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,10 +1,13 @@
-function solution(money) {
-    const price = 5500
-    let arr = []
+// function solution(money) {
+//     const price = 5500
+//     let arr = []
     
-    const max = Math.floor(money / price)
-    const minus = money - (max * price)
-    arr.push(max, minus)
+//     const max = Math.floor(money / price)
+//     const minus = money - (max * price)
+//     arr.push(max, minus)
     
-    return arr
+//     return arr
+// }
+const solution = (money) => {
+    return [Math.floor(money / 5500), money % 5500]
 }
