876197927a65433745adea01489f37f9c590c94c
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-07-30 23:23:54 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.12 ms, Memory: 44.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,10 @@
+const solution = (n) => {
+    const arr = []
+    for(let i = 1; i <= n; i++){
+        if(i % 2 === 1){
+            arr.push(i)
+        }
+    }
+    
+    return arr.sort((a, b) => a - b)
+}
