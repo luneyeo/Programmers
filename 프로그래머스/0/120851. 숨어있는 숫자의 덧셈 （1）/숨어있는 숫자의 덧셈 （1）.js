da1fa8a10d096782638967d7a1a91ae1dfedf56a
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-07-30 13:18:34 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.09 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,10 +1,14 @@
+// const solution = (string) => {
+//     const number = string.split("").filter((n) => n > 0)
+    
+//     let sum = 0;
+//     for(let i = 0; i < number.length; i++){
+//         sum += Number(number[i])
+//     }
+    
+//     return sum
+// }
+
 const solution = (string) => {
-    const number = string.split("").filter((n) => n > 0)
-    
-    let sum = 0;
-    for(let i = 0; i < number.length; i++){
-        sum += Number(number[i])
-    }
-    
-    return sum
+    return [...string].filter((n) => n > 0).reduce((a, c) => a + Number(c), 0)
 }
