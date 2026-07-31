87e0fe876c33022e80cc19ea2134e0ff8b6d6047
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-07-31 14:03:24 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.08 ms, Memory: 43.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,7 @@
+const solution = (num_list) => {
+  const last = num_list[num_list.length - 1];
+  const prev = num_list[num_list.length - 2];
+
+  last > prev ? num_list.push(last - prev) : num_list.push(last * 2);
+    return num_list
+};
