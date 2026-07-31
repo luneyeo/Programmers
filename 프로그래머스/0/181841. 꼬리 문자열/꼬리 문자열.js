5752052aa5c761e87cd9d944a36a633f7da533b2
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-07-31 23:50:34 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.09 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
@@ -1,4 +1,4 @@
 function solution(str_list, ex) {
-    const arr = str_list.filter((el) => !el.includes(ex))
-    return arr.join("")
+    const arr = str_list.filter((el) => !el.includes(ex)).join("")
+    return arr
 }
