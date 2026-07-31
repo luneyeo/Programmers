ff81738e7e2e0ae0e9440dcc9808f7de6201fe3e
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-07-31 13:40:09 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 1.37 ms, Memory: 45.5 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,11 +1,14 @@
-const solution = (my_string, index_list) => {
-    // 1. 문자열 배열로 변환
-    // 2. index_list의 값으로 배열의 인덱스 사용
-    const stringArr = my_string.split("")
-    let result = []
-    for(let i = 0; i <= index_list.length; i++){
-        const index = index_list[i]
-        result.push(stringArr[index])
-    }
-    return result.join("")
+// const solution = (my_string, index_list) => {
+//     // 1. 문자열 배열로 변환
+//     // 2. index_list의 값으로 배열의 인덱스 사용
+//     const stringArr = my_string.split("")
+//     let result = []
+//     for(let i = 0; i <= index_list.length; i++){
+//         const index = index_list[i]
+//         result.push(stringArr[index])
+//     }
+//     return result.join("")
+// }
+const solution = (string, index_list) => {
+    return index_list.map((i) => string.split("")[i]).join("")
 }
