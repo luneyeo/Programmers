a912b6481a48f4d63695c8ef71e065a5bf4cdd4c
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-07-31 23:27:21 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.06 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,8 +1,11 @@
+// const solution = (num_list) => {
+//     for(let i = 0; i < num_list.length; i++){
+//         if(num_list[i] < 0){
+//             return i;
+//         }
+//     }
+//     return -1
+// }
 const solution = (num_list) => {
-    for(let i = 0; i < num_list.length; i++){
-        if(num_list[i] < 0){
-            return i;
-        }
-    }
-    return -1
+    return num_list.findIndex((num) => num < 0)
 }
