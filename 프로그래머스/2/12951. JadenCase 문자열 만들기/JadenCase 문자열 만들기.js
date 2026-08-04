6d66ccc2d8f82515f91edb46524e72aecce8f6b6
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-08-04 15:35:04 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.13 ms, Memory: 44 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,21 @@
+function solution(s) {
+    let result = '';
+    let isFirst = true;
+    
+    for(let i = 0; i < s.length; i++){
+        let char = s[i];
+        
+        if(char === " "){
+            result += char;
+            isFirst = true;
+        } else {
+            if(isFirst){
+                result += char.toUpperCase();
+                isFirst = false;
+            } else {
+                result += char.toLowerCase();
+            }
+        }
+    }
+    return result
+}
