e037ad9e2436eb3722095ba9981e7921f86ea75b
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-08-05 14:31:11 +0900

    [level 1] Title: 행렬의 덧셈, Time: 2.93 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,14 @@
-function solution(arr1, arr2) {
-    const result = []
-    for(let i = 0; i < arr1.length; i++){
-        result[i] = []
-        for(let j = 0; j < arr1[i].length; j++){
-            result[i][j] = arr1[i][j] + arr2[i][j]
-        }
-    }
+// function solution(arr1, arr2) {
+//     const result = []
+//     for(let i = 0; i < arr1.length; i++){
+//         result[i] = []
+//         for(let j = 0; j < arr1[i].length; j++){
+//             result[i][j] = arr1[i][j] + arr2[i][j]
+//         }
+//     }
     
-    return result
+//     return result
+// }
+const solution = (arr1, arr2) => {
+    return arr1.map((el, i) => el.map((num, j) => num + arr2[i][j]))
 }
