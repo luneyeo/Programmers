8066e091e54722ddc1c2840d267aba4b2bfd9b17
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-08-05 13:15:18 +0900

    [level 0] Title: 배열 비교하기, Time: 0.10 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -1,13 +1,8 @@
 function solution(arr1, arr2) {
-    let result = 0;
+    if(arr1.length > arr2.length) return 1
+    if(arr1.length < arr2.length) return -1
     
-    if(arr1.length !== arr2.length){
-        result = arr1.length == arr2.length ? 0 : arr1.length > arr2.length ? 1 : -1
-    } else {
-        const sum1 = arr1.reduce((a, c) => a + c, 0)
-        const sum2 = arr2.reduce((a, c) => a + c, 0)
-        result = sum1 == sum2 ? 0 : sum1 > sum2 ? 1 : -1
-    }
-    
-    return result
+    const sum1 = arr1.reduce((a, c) => a + c, 0)
+    const sum2 = arr2.reduce((a, c) => a + c, 0)
+    return sum1 == sum2 ? 0 : sum1 > sum2 ? 1 : -1
 }
