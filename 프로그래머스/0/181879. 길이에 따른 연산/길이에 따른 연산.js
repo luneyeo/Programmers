f35bde5b4eae1f5bbf291e7144bc921c583c642b
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-08-08 20:04:56 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.09 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -1,10 +1,18 @@
-function solution(num_list) {
-    if(num_list.length >= 11){
-        const sum = num_list.reduce((a, c) => a + c, 0)
-        return sum
-    }
-    if(num_list.length <= 10){
-        const multiply = num_list.reduce((a, c) => a * c, 1)
-        return multiply                  
-    }
+// function solution(num_list) {
+//     if(num_list.length >= 11){
+//         const sum = num_list.reduce((a, c) => a + c, 0)
+//         return sum
+//     }
+//     if(num_list.length <= 10){
+//         const multiply = num_list.reduce((a, c) => a * c, 1)
+//         return multiply                  
+//     }
+// }
+const solution = (numList) => {
+    const mul = (a, c) => a * c
+    const sum = (a, c) => a + c
+    
+    return numList.length > 10
+            ? numList.reduce(sum, 0)
+            : numList.reduce(mul, 1)
 }
