a477a179809bff5943981cfbcfbbd3f7469c7b19
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-08-18 12:53:17 +0900

    [level 0] Title: 주사위 게임 2, Time: 2.14 ms, Memory: 44.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,7 +1,7 @@
 function solution(a, b, c) {
     const sum = a + b + c;
-    const square = a * a + b * b + c * c;
-    const cube = a * a * a + b * b * b + c * c * c;
+    const square = a ** 2 + b ** 2 + c ** 2;
+    const cube = a ** 3 + b ** 3 + c ** 3;
 
     if (a !== b && b !== c && a !== c) return sum;
     else if (a === b && b === c) return sum * square * cube;
