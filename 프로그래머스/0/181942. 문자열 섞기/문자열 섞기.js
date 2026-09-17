207a37f1bc3569484850fef186eae6717e550062
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-09-17 11:54:17 +0900

    [level 0] Title: 문자열 섞기, Time: 0.08 ms, Memory: 43.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -0,0 +1,10 @@
+function solution(str1, str2) {
+    let result = '';
+    
+    for(let i = 0; i < str1.length; i++){
+        result += str1[i];
+        result += str2[i];
+    }
+    
+    return result;
+}
